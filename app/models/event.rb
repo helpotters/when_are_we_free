648f41814b5264c85ec4e8a5bf0a3d921a87be99
Paul Lemus <paullemus@protonmail.com>
2022-04-23 19:38:17 -0700

    fix: allowed description to be empty

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   validates :title, length: { in: 2..100 }
-  validates :description, length: { in: 2..100 }
+  validates :description, length: { in: 0..100 }
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
