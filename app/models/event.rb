fa06bdc53cb35ee03ab1b02f6ffc76f107c8f353
Paul Lemus <paullemus@protonmail.com>
2022-04-23 19:56:36 -0700

    feat: added input cleaner helper for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,13 @@ class Event < ApplicationRecord
   validates :description, length: { in: 0..100 }
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  before_validation :clean_user_inputs, only: %i[title description]
+
+  private
+
+  def clean_user_inputs
+    self.title = title.delete("\000") unless title.nil?
+    self.description = description.delete("\000") unless description.nil?
+  end
 end
