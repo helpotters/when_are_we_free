04599b4dac1888e91f7bd752956db55040a4ba1a
Paul Lemus <paullemus@protonmail.com>
2022-04-24 18:45:53 -0700

    chore: changed faker data to harry potter quotes

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :event do
     title { Faker::Kpop.iii_groups }
-    description { Faker::String.random }
+    description { Faker::Movies::HarryPotter.quote }
     start_date { Date.today }
     end_date { Faker::Date.forward(days: 31) }
   end
