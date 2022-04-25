ba5f0545007976b324770690c943fbab02e14c48
Paul Lemus <paullemus@protonmail.com>
2022-04-25 10:35:10 -0700

    refactor: simplified checks for test_ids

--- spec/features/event_show_spec.rb
+++ spec/features/event_show_spec.rb
@@ -2,26 +2,18 @@ require 'rails_helper'
 
 RSpec.feature 'EventShow', type: :feature do
   let(:event) { create(:event) }
-  let(:title) { event.title }
-  let(:description) { event.description }
-  let(:start_date) { event.start_date }
-  let(:end_date) { event.end_date }
+
   before do
     visit "events/#{event.id}"
   end
+  def self.view_content(attribute)
+    it "check for #{attribute} in event view" do
+      value = event[attribute]
+      expect(find(:test_id, "event_#{attribute}")).to have_content(value)
+    end
+  end
 
   context 'View Basic Information' do
-    it 'should have event title' do
-      expect(find(:test_id, 'event_title')).to have_content(title)
-    end
-    it 'should have event description' do
-      expect(find(:test_id, 'event_description')).to have_content(description)
-    end
-    it 'should have event start date' do
-      expect(find(:test_id, 'event_start_date')).to have_content(start_date)
-    end
-    it 'should have event end date' do
-      expect(find(:test_id, 'event_end_date')).to have_content(end_date)
-    end
+    %w[title description start_date end_date].each { |attribute| view_content(attribute) }
   end
 end
