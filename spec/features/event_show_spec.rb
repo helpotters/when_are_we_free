96a6f9f4075aa37887faf683be6816c3db7495ee
Paul Lemus <paullemus@protonmail.com>
2022-04-24 18:43:17 -0700

    test: check for event title and description

--- spec/features/event_show_spec.rb
+++ spec/features/event_show_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 RSpec.feature 'EventShow', type: :feature do
   subject(:event) { create(:event) }
+  let(:title) { event.title }
+  let(:description) { event.description }
   before do
-    visit "/events/#{event.id}/show"
+    visit "events/#{event.id}"
+  end
+
+  context 'View Basic Information' do
+    it 'should have event title' do
+      expect(find(:test_id, 'event_title')).to have_content(title)
+      expect(find(:test_id, 'event_description')).to have_content(description.to_s)
+    end
   end
 end
