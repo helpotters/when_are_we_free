eef6a3123a365e758b04a1d48154448f27e7bfe2
Paul Lemus <paullemus@protonmail.com>
2022-04-25 14:08:43 -0700

    test: added test for invalid vote form

--- spec/features/voting_form_spec.rb
+++ spec/features/voting_form_spec.rb
@@ -2,13 +2,37 @@ require 'rails_helper'
 
 RSpec.feature 'VotingForms', type: :feature do
   let(:event) { create(:event) }
+  let(:duration) { event.start_date..event.end_date }
+  let(:name) { Faker::JapaneseMedia::DragonBall.character }
 
   before do
-    visit "events/#{event.id}"
+    visit 'voters/new.html.erb'
+  end
+
+  # Can select any day in calendar
+  def self.fill_forms(availability)
+    it 'select days in calendar' do
+      page.all(:test_id, 'day').each do |day|
+        day.click if availability.include?(day.value)
+      end
+    end
   end
 
   context 'does not fill required fields' do
+    it 'fills in name but not calendar' do
+      not_available = nil
+      find(:test_id, 'name').fill_in with(nil)
+
+      expect(page).to have_content('cannot be empty')
+    end
   end
-  context 'does fill required fields' do
+  xcontext 'does fill required fields' do
+    it 'fills in name and calendar' do
+      availability = duration.sample
+
+      find(:test_id, 'name').fill_in with(:name)
+
+      fill_forms(availability)
+    end
   end
 end
