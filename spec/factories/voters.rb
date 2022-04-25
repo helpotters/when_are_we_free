f66bf4eee6d8490c99a779a79a12db40fc703366
Paul Lemus <paullemus@protonmail.com>
2022-04-25 16:06:43 -0700

    test: added model test for voter

--- spec/factories/voters.rb
+++ spec/factories/voters.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :voter do
-    
+    name { Faker::Movies::HarryPotter.character }
+    event { create(:event) }
   end
 end
--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Voter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:voter) { create(:voter) }
+
+  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(15) }
+  it { is_expected.to belong_to(:event) }
+  it { is_expected.to have_many(:votes) }
 end
