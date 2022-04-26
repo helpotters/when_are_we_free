c767fbefca4be6e57cd52be5c5dfcf3a26b63568
Paul Lemus <paullemus@protonmail.com>
2022-04-25 23:14:31 -0700

    feat: added voter model

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,4 +1,7 @@
 class Voter < ApplicationRecord
+  validates_presence_of :name
+  validates_length_of :name, minimum: 1, message: 'is too short (minimum is 1 character)'
+  validates_length_of :name, maximum: 26, message: 'is too long (maximum is 26 characters)'
   belongs_to :event
   has_many :votes
 end
--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Voter, type: :model do
   subject(:voter) { create(:voter) }
 
-  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(15) }
+  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(26) }
   it { is_expected.to belong_to(:event) }
   it { is_expected.to have_many(:votes) }
 end
