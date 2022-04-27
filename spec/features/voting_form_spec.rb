ff0530897f323a107e70a6767cd8f7bc56d67e3f
Paul Lemus <paullemus@protonmail.com>
2022-04-27 16:39:55 -0700

    spec: added voter form spec for just name inputs
    It currently allows for submissions with no availability indicated, if
    given a valid name.

--- spec/features/voting_form_spec.rb
+++ spec/features/voting_form_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature 'VotingForms', type: :feature do
+  let(:event) { create(:event) }
+  let(:duration) { event.start_date..event.end_date }
+  let(:availability) { duration.map(&:to_a).sample(rand(1..duration.count)) }
   before do
     visit event_url(event.id)
+    find(:test_id, 'name').fill_in(with: Faker::Movies::LordOfTheRings.character)
   end
-  context 'does fill required fields' do
+  context 'does not fill in availability' do
+    it 'just submits name' do
+      find(:test_id, 'submit').click
+      expect(Voter.last.votes.count).to eq(0)
+    end
   end
-  context 'does not fill required fields' do
+  context 'does fill in availability' do
+    it 'fills in every day of availability' do
+      days = page.all(id: '#calendar_day')
+      days.each do |day|
+        day.click if availability.include?(day.value)
+      end
+
+      find(:test_id, 'submit').click
+      expect(Voter.last.votes.count).to eq(availability.count)
+    end
   end
 end
