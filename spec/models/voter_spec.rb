7f3fb26c00102b7f24208d07a914a94886648d53
Paul Lemus <paullemus@protonmail.com>
2022-04-27 17:36:46 -0700

    spec: added expectation for nested_attributes

--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe Voter, type: :model do
   subject(:voter) { create(:voter) }
 
-  it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(26) }
-  it { is_expected.to belong_to(:event) }
-  it { is_expected.to have_many(:votes) }
+  context 'validations' do
+    it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(26) }
+  end
+  context 'associations' do
+    it { is_expected.to belong_to(:event) }
+    it { is_expected.to have_many(:votes) }
+  end
+  context 'attributes' do
+    it { is_expected.to accept_nested_attributes_for(:votes) }
+  end
 end
