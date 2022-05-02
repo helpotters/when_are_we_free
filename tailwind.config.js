d8d1858145d403cdfb36ff414531acf6bec46ed4
Paul Lemus <paullemus@protonmail.com>
2022-05-01 19:06:40 -0700

    styling: added karla font

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
         extend: {
           fontFamily: {
                 Indie: ["Indie Flower", "cursive"],
+                Karla: ["Karla", "sans-serif"],
           },
         },
         neumorphismColor: {
