65f44e366dbe9b4b297dc262135d2336b58b27b8
Paul Lemus <paullemus@protonmail.com>
2022-12-03 22:22:33 -0800

    fix: updated render settings for assets

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 bundle install
-bundle exec rake assets:precompile
+bundle exec rails assets:precompile RAILS_ENV=production
 bundle exec rake assets:clean
 bundle exec rake db:migrate
