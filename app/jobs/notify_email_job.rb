064d2407bfafb0731e7fd4b5125eae482a8033a1
Paul Lemus <paullemus@protonmail.com>
2022-12-09 12:50:57 -0800

    feat: added invalid case for notify_email job

--- app/jobs/notify_email_job.rb
+++ app/jobs/notify_email_job.rb
@@ -3,6 +3,7 @@
 class NotifyEmailJob < ApplicationJob
   def perform(voter_id)
     voter = Voter.find_by(id: voter_id)
-    VoterMailer.send_confirmation_email_to(voter).deliver_now!
+    p voter
+    VoterMailer.send_confirmation_email_to(voter).deliver_now! if voter.present?
   end
 end
