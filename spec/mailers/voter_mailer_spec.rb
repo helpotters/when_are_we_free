1bbd865cb6979f2ba1d34259fd07d1f3363499a6
Paul Lemus <paullemus@protonmail.com>
2022-12-09 18:58:38 -0800

    spec: added expectations to test

--- spec/mailers/voter_mailer_spec.rb
+++ spec/mailers/voter_mailer_spec.rb
@@ -9,16 +9,18 @@ RSpec.describe VoterMailer do
     let(:voter) { create(:voter, email: 'test@example.com') }
 
     it 'renders the correct sender address' do
-      expect(voter_email.from).to eql(['confirm@whenarewefree.com'])
+      expect(voter_email.from).to eq(['confirm@whenarewefree.com'])
     end
 
     it 'renders the correct subject' do
+      expect(voter_email.subject).to eq('The votes are in!')
     end
 
     it 'renders the correct days' do
     end
 
     it 'send the email to the correct address' do
+      expect(voter_email.to).to eq(['test@example.com'])
     end
   end
 end
