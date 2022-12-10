64d72915426b88fef6c3b511740bde842e9d6d8c
Paul Lemus <paullemus@protonmail.com>
2022-12-09 18:06:58 -0800

    refactor: removed puts statement

--- app/jobs/notify_email_job.rb
+++ app/jobs/notify_email_job.rb
@@ -3,7 +3,6 @@
 class NotifyEmailJob < ApplicationJob
   def perform(voter_id)
     voter = Voter.find_by(id: voter_id)
-    p voter
     VoterMailer.send_confirmation_email_to(voter).deliver_now! if voter.present?
   end
 end
