16ed45bdf3deba9a76982f527c7eaff0bf11c0cf
Paul Lemus <paullemus@protonmail.com>
2022-12-09 19:02:03 -0800

    feat: added to:email to confirmation email

--- app/mailers/voter_mailer.rb
+++ app/mailers/voter_mailer.rb
@@ -1,8 +1,9 @@
 class VoterMailer < ApplicationMailer
   default from: 'confirm@whenarewefree.com'
-  def send_confirmation_email_to(_voter)
+  def send_confirmation_email_to(voter)
     mail(
       subject: 'The votes are in!',
+      to: voter.email,
       template_name: 'confirm_session'
     )
   end
