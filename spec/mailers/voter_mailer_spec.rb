ba43a3936618b15f02854c0f4287e18938226fcf
Paul Lemus <paullemus@protonmail.com>
2022-12-13 16:04:09 -0800

    spec: expect majority days to be in email

--- spec/mailers/voter_mailer_spec.rb
+++ spec/mailers/voter_mailer_spec.rb
@@ -4,9 +4,13 @@ require 'rails_helper'
 
 RSpec.describe VoterMailer do
   describe '#send_confirmation_email_to' do
-    subject(:voter_email) { described_class.send_confirmation_email_to(voter) }
+    subject(:voter_email) { described_class.send_confirmation_email_to(@voter) }
 
-    let(:voter) { create(:voter, email: 'test@example.com') }
+    before(:each) do
+      @event = create(:event)
+      @voter = create(:confirmed_voter, email: 'test@example.com', event: @event)
+      @voters = create_list(:confirmed_voter, 5, event: @event)
+    end
 
     it 'renders the correct sender address' do
       expect(voter_email.from).to eq(['confirm@whenarewefree.com'])
@@ -17,6 +21,11 @@ RSpec.describe VoterMailer do
     end
 
     it 'renders the correct days' do
+      majorities = @voter.event.majority(@event.id)
+
+      majorities.each do |day|
+        expect(voter_email.body.encoded).to include(day[0][1].to_s)
+      end
     end
 
     it 'send the email to the correct address' do
