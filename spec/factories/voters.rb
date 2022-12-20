3ff85512cd4914e64d9f4fe7d388bfc807cd210c
Paul Lemus <paullemus@protonmail.com>
2022-12-20 12:58:18 -0800

    fix: moved email value to confirmed_voter

--- spec/factories/voters.rb
+++ spec/factories/voters.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :voter do
     name { Faker::Movies::HarryPotter.character }
     event { create(:event) }
-    email { Faker::Internet.email }
 
     factory :confirmed_voter do
+      email { Faker::Internet.email }
       after :create do |confirmed|
         create(:vote, voter: confirmed, event: confirmed.event,
                       day: confirmed.event.start_date + 4)
