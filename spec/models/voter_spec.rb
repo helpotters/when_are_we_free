d4b30ff0c5c69489c060025fd75acc8ff2afbd78
Paul Lemus <paullemus@protonmail.com>
2022-12-20 22:14:06 -0800

    feat: limit email uniqueness to event

--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Voter, type: :model do
 
   context 'validations' do
     it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(26) }
-    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { is_expected.to validate_uniqueness_of(:email).scoped_to(:event_id).ignoring_case_sensitivity }
     it { is_expected.to allow_value('example@email.com').for(:email) }
     it { is_expected.not_to allow_value('not_email').for(:email) }
   end
