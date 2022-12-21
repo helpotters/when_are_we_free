63e0a34c48279334302e6e37e22db6640541c9fa
Paul Lemus <paullemus@protonmail.com>
2022-12-20 16:35:31 -0800

    feat: accept minimum param for event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :start_date, :end_date)
+    params.require(:event).permit(:title, :description, :minimum, :start_date, :end_date)
   end
 end
