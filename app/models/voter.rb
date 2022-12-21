b7e8205067fb477496485e3ffb98e556fa3bd4f2
Paul Lemus <paullemus@protonmail.com>
2022-12-20 16:24:51 -0800

    feat: send email after voter updates/creates

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,4 +1,7 @@
 class Voter < ApplicationRecord
+  after_create :check_if_minimum_met
+  after_update :check_if_minimum_met
+
   validates :name, presence: :true, uniqueness: { scope: :event_id }
   validates_length_of :name, minimum: 1, message: 'is too short (minimum is 1 character)'
   validates_length_of :name, maximum: 26, message: 'is too long (maximum is 26 characters)'
@@ -11,4 +14,13 @@ class Voter < ApplicationRecord
   accepts_nested_attributes_for :votes, allow_destroy: true, reject_if: proc { |vote|
                                                                           true unless vote.key?('day')
                                                                         }
+
+  private
+
+  def check_if_minimum_met
+    event = Event.find_by(id: event_id)
+    voters = event.voters.count
+    minimum = event.minimum
+    NotifyEmailJob.new.email_event_members(event_id) if voters >= minimum
+  end
 end
