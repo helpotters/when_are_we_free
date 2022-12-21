503030afb6f53273e5c1966a58d7f40f3e28d805
Paul Lemus <paullemus@protonmail.com>
2022-12-20 18:19:11 -0800

    feat: added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  domain: 'whenarewefree.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
