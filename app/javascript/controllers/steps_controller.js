bff116382064fbc8abc50f53b8894f9df1f0444c
Paul Lemus <paullemus@protonmail.com>
2022-12-25 18:42:39 -0800

    feat: progress steps bar to confirm

---
+++ app/javascript/controllers/steps_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="steps"
+export default class extends Controller {
+  static targets = ["stepsList"]
+
+  connect() {
+    document.addEventListener("turbo:before-visit", (event) => {
+    })
+  }
+
+  updateSteps(event) {
+    let stepCount = event.params.step
+    console.log(stepCount)
+    console.log(event.params)
+    let steps = this.stepsListTarget.children
+    let i = 0
+    while(i<=stepCount){
+      steps[i].classList.add('step-neutral')
+      steps[i].setAttribute('id', i)
+      i++
+    }
+    event
+  }
+  updateLastStep(event){
+    document.getElementById("last").classList.add('step-neutral')
+  }
+}
